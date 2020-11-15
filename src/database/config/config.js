f315c848baf2695e9395adf95809da38cd1d58bc
mansurissa <mansurissa6@gmail.com>
2020-11-15 20:13:34 +0200

    chore: configure app to use postgres database

---
+++ src/database/config/config.js
@@ -0,0 +1,17 @@
+import { config } from 'dotenv';
+config();
+
+module.exports = {
+  development: {
+    url: process.env.DEV_DATABASE_URL,
+    dialect: 'postgres',
+  },
+  test: {
+    url: process.env.TEST_DATABASE_URL,
+    dialect: 'postgres',
+  },
+  production: {
+    url: process.env.DATABASE_URL,
+    dialect: 'postgres',
+  },
+};
