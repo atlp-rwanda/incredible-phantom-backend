763249be6d5850f1994c19da1bf8e329b4645787
mansurissa <mansurissa6@gmail.com>
2020-11-15 20:13:34 +0200

    chore: connect to database
    - install express
    - install dotenv
    - making test route
    
    ch-welcome route(welcome route): seting up welcome route
    
     chore: connect to database
    
    ch-setup(initial-setup):setting up empty project
    
    - swagger documentaion setup
    - eslint setup
    - babel
    - .env.example for referencing .env
    - folder structure
    - welcome endpiont
    
    chore Added initial translating route
    
    Changing languages from user input
    
    chore: Connect to  database

--- src/index.js
+++ src/index.js
@@ -2,14 +2,20 @@ import express, { json } from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> 1432494... chore: connect to database
 import swagger from 'swagger-ui-express';
 import swaggerDoc from '../documentation/swaggerDoc.json';
 import { config } from 'dotenv';
 import router from './routers/index';
 import bodyParser from 'body-parser';
+<<<<<<< HEAD
 =======
 import { config } from 'dotenv';
 >>>>>>> 566e629... ch-setup(initial setup): setting up project
+=======
+>>>>>>> 1432494... chore: connect to database
 
 config();
 
@@ -17,6 +23,7 @@ const app = express();
 app.use(morgan('dev'));
 app.use(cors());
 app.use(json());
+<<<<<<< HEAD
 <<<<<<< HEAD
 app.use(bodyParser.urlencoded({extended : true}));
 app.use(express.static('public'));
@@ -30,10 +37,14 @@ app.listen(3000, console.log(`Server started on port ${port}`));
 
 export default app;
 =======
+=======
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(express.static('public'));
+>>>>>>> 1432494... chore: connect to database
 
-app.get('/users', (req, res) => {
-  res.send('Hello phantom we are comming for you');
-});
+app.use('/api/documentation', swagger.serve, swagger.setup(swaggerDoc));
+
+app.use('/api', router);
 
 const port = process.env.PORT;
 app.listen(3000, console.log(`Server started on port ${port}`));
