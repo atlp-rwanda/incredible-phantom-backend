8414221f7c7d8030d39a5e29eb8d003b794cc79c
GuevaraMarius <irabizimguevara@gmail.com>
2020-11-17 20:35:17 +0200

    feat(authentication):signin
    - create user model and migration
    - create seeder users
    - hash password
    - sign in
    - asign token to signed in user
    
    chore:(eslint): configuringneslint
    
    - install and configure eslint
    - using airbnb base style
    
    chore: connect-to-db
    - creating seeds
    - creating migration
    - user model
    
     bg-fixing some model error
    
    ft: signin
    
    - registering user
    - validations with join
    
     hashing pass2word before saving in db
    
    feat(authenticatiom):sign in
    
    - login for user
    - signing token to signed in user

--- src/routers/index.js
+++ src/routers/index.js
@@ -2,6 +2,10 @@ import { Router } from 'express';
 import welcome from '../controllers/welcome';
 import translation from '../controllers/i18n';
 
+import translate from '../controllers/translate';
+import userRouter from './usersRouter';
+
+
 const router = Router();
 
 router.get('/', welcome);
