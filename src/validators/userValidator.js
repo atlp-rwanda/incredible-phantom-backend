b966fca1c8535d307092a5ee2ec8add1037f8812
KundwaBruno <kundwabruno@gmail.com>
2020-11-20 10:25:28 +0200

    Using google cloud transalte api for supporting multi languages
    Changed to ES6 syntax
    
    Fix issue #15
    
    Fixed Issue_#15'
    
    issue_#15
    
    Fixed issue #15 and i18n setup
    
    xUjJi1sq Chore Support multiple languages
    
    ch-CRUD-role
    
    CREATE Roles
    READ Roles
    UPDATE Roles
    DELETE Roles
    
    ch-permissions-middleware
    
    Fix issue #15
    
    Fixed Issue_#15'
    
    issue_#15
    
    Fixed issue #15 and i18n setup
    
    xUjJi1sq Chore Support multiple languages
    
    ch-setup(initial setup): setting up project
    
    - install express
    - install dotenv
    - making test route
    
    feat(authentication):signin
    
    - create user model and migration
    - create seeder users
    - hash password
    - sign in
    - asign token to signed in user
    
    chore:(eslint): configuringneslint
    
    - install and configure eslint
    - using airbnb base style
    
    chore: connect-to-db
    - creating seeds
    - creating migration
    - user model
    
     bg-fixing some model error
    
    ft: signin
    
    - registering user
    - validations with join
    
     hashing pass2word before saving in db
    
    feat(authenticatiom):sign in
    
    - login for user
    - signing token to signed in user
    
    ch-setup(initial setup): setting up project
    
    - install express
    - install dotenv
    - making test route
    
    feat(authentication):signin
    
    - create user model and migration
    - create seeder users
    - hash password
    - sign in
    - asign token to signed in user
    
    chore:(eslint): configuringneslint
    
    - install and configure eslint
    - using airbnb base style
    
    chore: connect-to-db
    - creating seeds
    - creating migration
    - user model
    
     bg-fixing some model error
    
    ft: signin
    
    - registering user
    - validations with join
    
     hashing pass2word before saving in db
    
    feat(authenticatiom):sign in
    
    - login for user
    - signing token to signed in user
    
    feat(register) registring users
    
    - random password
    - sending email(not yet done)
    
     generating random password
    
     sending email
    
    verify endpoint
    
     creating midleware for checking token
    
    feat:(register): registering users
    
    - registering user
    - sending email after creating user
    - sending comfirmation email after verifying account
    
    ElYrZR34

--- src/validators/userValidator.js
+++ src/validators/userValidator.js
@@ -3,12 +3,11 @@ import Joi from 'joi';
 export const RegisterValidator = Joi.object({
   firstName: Joi.string().min(2).max(64).required(),
   lastName: Joi.string().min(2).max(64).required(),
-  password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')).required(),
+  password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')),
   nationalId: Joi.number().min(16).required(),
   language: Joi.string(),
   phone: Joi.string().min(10).max(13),
   role: Joi.string().required(),
-  language: Joi.string().default('en'),
   email: Joi.string()
     .required()
     .email({
