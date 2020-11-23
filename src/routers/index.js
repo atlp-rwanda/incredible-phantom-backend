75c4d8be7f5458a985c75964c98e5d48e8d17606
KundwaBruno <kundwabruno@gmail.com>
2020-11-23 11:17:50 +0200

    Fix issue #15
    Fixed Issue_#15'
    
    issue_#15
    
    Fixed issue #15 and i18n setup
    
    xUjJi1sq Chore Support multiple languages

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,10 +1,23 @@
 import { Router } from 'express';
 import welcome from '../controllers/welcome';
+
+import translate from '../controllers/translate';
+
 import userRouter from './usersRouter';
+
+import path from 'path'
+import translation from '../controllers/i18n'
+
+
 
 const router = Router();
 
 router.get('/', welcome);
+router.post('/translate', translate);
+
 router.use('/users', userRouter);
+
+router.get('/translate' , translation);
+
 
 export default router;
