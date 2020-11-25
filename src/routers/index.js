483a6c2cb0e5fe2a36ec5d690ada80653595df07
mansurissa <mansurissa6@gmail.com>
2020-11-25 07:34:21 +0200

    feat(register) registring users
    - random password
    - sending email(not yet done)

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 import welcome from '../controllers/welcome';
+
 import translation from '../controllers/i18n';
 
 import translate from '../controllers/translate';
-import userRouter from './usersRouter';
 
+import userRouter from './usersRouter';
 
 const router = Router();
 
