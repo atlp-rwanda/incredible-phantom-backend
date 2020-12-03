b81e4d02815206e568a2e9efb0764e48de5bd1d2
isabwe egide <egide.isabwe@gmail.com>
2020-12-03 18:32:53 +0200

    changes

---
+++ documentation/swaggerDoc.js
@@ -0,0 +1,36 @@
+const express=require('express');
+  const swaggerJsdoc = require("swagger-jsdoc");
+  const swaggerUi = require("swagger-ui-express");
+  const docrouter=express.Router();
+
+const options = {
+    definition: {
+      openapi: "3.0.0",
+      info: {
+        title: "Phantom Express API with Swagger",
+        version: "0.1.0",
+        description:
+          "This is a simple CRUD API application made with Express and documented with Swagger",
+        
+        contact: {
+          name: "Phantom",
+          url: "",
+          email: "atlpincredible@gmail.com",
+        },
+      },
+      servers: [
+        {
+          url: "http://localhost:3000",
+        },
+      ],
+    },
+    apis: ["../src/routers/*.js"],
+  };
+  
+  const specs = swaggerJsdoc(options);
+  docrouter.use(
+    "/",
+    swaggerUi.serve,
+    swaggerUi.setup(specs)
+  );
+  export default docrouter
--- src/index.js
+++ src/index.js
@@ -2,25 +2,35 @@ import express, { json } from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
 import swagger from 'swagger-ui-express';
-import swaggerDoc from '../documentation/swaggerDoc.json';
+import swaggerDoc from '../documentation/swaggerDoc';
 import { config } from 'dotenv';
 import router from './routers/index';
-import bodyParser from 'body-parser';
+
+import Docrouter from '../documentation/swaggerDoc';
+const socket = require("socket.io");
 
 config();
-
 const app = express();
+
 app.use(morgan('dev'));
 app.use(cors());
 app.use(json());
-app.use(bodyParser.urlencoded({extended : true}));
+
 app.use(express.static('public'));
 
-app.use('/api/documentation', swagger.serve, swagger.setup(swaggerDoc));
+app.use('/api/documentation', Docrouter);
+
 
 app.use('/api', router);
 
 const port = process.env.PORT;
-app.listen(port, console.log(`Server started on port ${port}`));
-
+const server=app.listen(port, console.log(`Server started on port ${port}`));
+const io=socket(server)
+io.on('connection', (socket) => {
+  console.log('User connected');
+  socket.on("disconnect", () => {
+      console.log("user disconnected");
+    });
+});
 export default app;
+
