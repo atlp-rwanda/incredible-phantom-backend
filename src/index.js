7bfd7fc7a13d8606e715ef677f14ce0520ecd71d
isabwe egide <egide.isabwe@gmail.com>
2020-12-03 18:32:53 +0200

    changes

--- src/index.js
+++ src/index.js
@@ -7,9 +7,12 @@ import { config } from 'dotenv';
 import router from './routers/index';
 import docRouter from './documentation/index';
 
-config();
+import Docrouter from '../documentation/swaggerDoc';
+const socket = require("socket.io");
 
+config();
 const app = express();
+
 app.use(morgan('dev'));
 app.use(cors());
 app.use(json());
@@ -24,6 +27,13 @@ app.get('/', (req, res) => {
 app.use('/api', router);
 
 const port = process.env.PORT;
-app.listen(port, console.log(`Server started on port ${port}`));
-
+const server=app.listen(port, console.log(`Server started on port ${port}`));
+const io=socket(server)
+io.on('connection', (socket) => {
+  console.log('User connected');
+  socket.on("disconnect", () => {
+      console.log("user disconnected");
+    });
+});
 export default app;
+
