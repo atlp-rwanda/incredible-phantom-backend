ee7985bd195735ebb3beef7c8c465becf4f05acf
Samuel Nishimwe <ceelogre@outlook.com>
2020-12-17 11:32:44 +0200

    Create node.js.yml
    This will setup GitHub Actions for the repo.
    
    chore(actions) initial db config setup
    
    chore(actions) add create db command
    
    chore(actions) map port from container to host
    
    chore(actions) set health checks to wait until for postgres to start
    
    chore(actions) remove createdb step
    
    chore(actions) set postgres host instead of runner
    
    chore(actions) run postgres client
    
    chore(actions) add the client script
    
    chore(actions) revert to commonjs
    
    chore(actions) run job in a container
    
    chore(actions) remove duplicate env HOST
    
    chore(actions) remove ports due to containerization
    
    chore(actions) add postgres installation
    
    chore(actions) run the command as non-root in docker
    
    chore(actions) run a docs example
    
    chore(actions) rewrite postgres installation commands
    
    chore(actions) create db with npx
    
    chore(actions) run job on runner machine
    
    chore(actions) add username property to config
    
    chore(actions) add password property to config
    
    chore(actions) set env before running scripts
    
    chore(actions) add redis image
    
    chore(actions) add redis entrypoint
    
    Update node.js.yml
    
    removing redis server
    
    Update node.js.yml
    
    removing redis
    
    Update node.js.yml
    
    reverting changes
    
    Update node.js.yml
    
    removing redis
    
    chore(actions) increase trials
    
    chore(actions) add postgres password env
    
    chore(actions) re-add redis
    
    chore(actions) add default jwt secret
    
    chore(actions) cache node modules for faster builds
    
    chore(actions) remove irrelevant client script

--- src/helpers/signToken.js
+++ src/helpers/signToken.js
@@ -4,6 +4,6 @@ import { config } from 'dotenv';
 config();
 
 export default (data) => {
-  const token = jwt.sign(data, process.env.JWT_KEY);
+  const token = jwt.sign(data, process.env.JWT_KEY || 'thisShouldWork');
   return token;
 };
